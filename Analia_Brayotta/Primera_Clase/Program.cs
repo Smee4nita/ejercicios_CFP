2955d3732ee04365ced46f9c6e5d6331140da17e
Smee4nita <b.analia.laura@gmail.com>
2024-03-19 22:52:55 -0300

    mi primer ejercicio

---
+++ Analia_Brayotta/Primera_Clase/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Primera_Clase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string apellido;
+            string edadTexto;
+            int edadNumerica;
+
+            Console.Write("Ingrese su nombre: ");
+           
+            nombre = Console.ReadLine();
+
+            Console.Write("Ingrese su apellido:");
+
+            apellido = Console.ReadLine();
+
+            Console.Write("Ingrese su edad: ");
+
+            edadTexto = Console.ReadLine();
+            
+            edadNumerica = int.Parse(edadTexto);
+
+
+            //Console.WriteLine("Bienvenido " + nombre + apellido + ", usted tiene: " + edadNumerica + " años");  FORMA STANDARD CLASICA DE CONCATENAR
+
+            //Console.WriteLine("Bienvenido {0} {1}, usted tiene: {2} años.", nombre, apellido, edadNumerica);    
+
+            Console.WriteLine($"Bienvenido {nombre} {apellido}, usted tiene: {edadNumerica} años.");
+
+
+
+
+
+        }
+    }
+}
